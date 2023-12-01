483c1fc0669168157eb5ba0b77a10fff86013e95
Iamneo <techadmin@iamneo.ai>
2023-12-01 08:18:06 +0000

    Auto-save-commit-12/1/2023--1:48:04-PM

--- springapp/src/main/java/com/examly/springapp/exceptions/ExceedsTeamBudgetException.java
+++
@@ -1,8 +0,0 @@
-package com.examly.springapp.exceptions;
-
-public class ExceedsTeamBudgetException extends RuntimeException {
-    public ExceedsTeamBudgetException(String message) {
-        super(message);
-    }
-}
-
--- springapp/src/main/java/com/examly/springapp/exceptions/PlayerAlreadyAssignedException.java
+++
@@ -1,7 +0,0 @@
-package com.examly.springapp.exceptions;
-
-public class PlayerAlreadyAssignedException extends RuntimeException {
-    public PlayerAlreadyAssignedException(String message) {
-        super(message);
-    }
-}
